0205c180214b26ff58223ad41bef258257ff637c
Muhammad Shayan <shayanarif666@gmail.com>
2025-05-03 22:46:01 +0500

    backend

--- models/Testimonial.Model.js
+++ models/Testimonial.Model.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 
-const Testimonial = new mongoose.Schema({
-  name: {
-    en: { type: String, required: true },
-    ar: { type: String, required: true },
-    fr: { type: String, required: true },
+const Testimonial = new mongoose.Schema(
+  {
+    name: {
+      en: { type: String, required: true },
+      ar: { type: String, required: true },
+      fr: { type: String, required: true },
+    },
+    rating: { type: Number, required: true },
+    image: { type: String, required: true },
+    review: {
+      en: { type: String, required: true },
+      ar: { type: String, required: true },
+      fr: { type: String, required: true },
+    },
   },
-  rating: { type: Number, required: true },
-  image: { type: String, required: true },
-  review: {
-    en: { type: String, required: true },
-    ar: { type: String, required: true },
-    fr: { type: String, required: true },
-  },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
 module.exports = new mongoose.model("Testimonial", Testimonial);
