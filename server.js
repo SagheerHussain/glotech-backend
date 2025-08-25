89e2db67bc4a3d4c8cd447572c5bfec08e744927
Muhammad Shayan <shayanarif666@gmail.com>
2025-08-25 13:29:11 +0500

    Bug Fixing

--- server.js
+++ server.js
@@ -68,6 +68,6 @@ app.use("/api/category", categoryRoute);
 app.use("/api/user", userRoute);
 
 // Server Listen
-app.listen(PORT, () => { 
+app.listen(PORT, '0.0.0.0', () => { 
   console.log(`Server is running on port ${PORT}`);
 });
